f02a92b7586ef6abb02b23eac5c5ce84b1b92dbb
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2022-05-05 20:34:54 -0700

    Updating Code

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ fn main() {
     println!("Chemical Equation Balancer in {{RUST}} (v1.0)");
     println!("For help, type .help");
     println!("Input Chemical Equation: ");
-    let x: i8 = 2;
     let mut s = String::new();
     let _ = stdin().read_line(&mut s);
     s.pop();
+    let v: Vec<&str> = s.split(" -> ").collect();
+    let reactants = v[0];
+    let products = v[1];
 }
